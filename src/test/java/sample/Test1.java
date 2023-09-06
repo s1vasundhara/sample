6849e3035b4b66a6fd99d142d223251954f1eaf7
admin <admin@vasundhara>
2023-09-06 14:02:01 +0530

    sample commit

---
+++ src/test/java/sample/Test1.java
@@ -0,0 +1,19 @@
+package sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test1 {
+	
+		@Test
+		public void test1()
+		{
+			Reporter.log("test1");
+		}
+		public void test2()
+		{
+			Reporter.log("test2");
+		}
+
+
+}
---
+++ src/test/java/sample/Test2.java
@@ -0,0 +1,17 @@
+package sample;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class Test2 {
+	@Test
+	public void test3()
+	{
+		Reporter.log("test3");
+	}
+	public void test4()
+	{
+		Reporter.log("test4");
+	}
+
+}
